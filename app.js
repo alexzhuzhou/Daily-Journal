682df23a73ee8c15c2c964256fa0f84c17768a35
Tian Hong Zhu Zhou <alexzhuzhou@gmail.com>
2025-01-08 12:49:38 -0800

    added put and delete features

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 //jshint esversion:6
 
 const express = require("express");
+const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // Load the full build.
 var _ = require('lodash');
 
- 
+const mongoose = require("mongoose");
 
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Unleash your thoughts, share your stories, and let your creativity flow. This is a space where your words matter. Whether it’s a random thought, a creative idea, or a story you’ve been waiting to share, this is your platform to connect with others and express yourself freely.";
+const aboutContent = "At The Insightful Blog , we believe everyone has a story to tell, an idea to share, or a thought worth exploring. Our platform is a space where creativity meets community—a place where users can post freely, discover diverse perspectives, and connect with like-minded individuals. Whether you're here to share a random thought, pen a short story, or simply browse what others have to say, The Insightful Blog is your digital diary and creative canvas rolled into one.";
+const contactContent = "We’d love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out to us.";
 
 const app = express();
 
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("public")); 
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+// create collection Schema
+const postSchema = {
+  title : String,
+  content: String
+}
 
 
-app.get("/", function(req, res) {
-  res.render("home", {startingContent: homeStartingContent, posts:posts});
+// create collection
+const Post = mongoose.model("Post", postSchema);
 
 
+
+
+app.get("/home", function(req, res) {
+  Post.find()
+    .then((posts) => {
+      if (posts.length === 0) {
+        res.render("home" ,{startingContent: homeStartingContent, posts:[]})
+      }
+      else {
+        res.render("home", {startingContent: homeStartingContent, posts:posts});
+      }
+
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 });
 
 app.get("/about", function(req, res) {
@@ -43,33 +66,58 @@ app.get("/compose", function(req, res) {
 })
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post = new Post({ 
     title: req.body.postTitle,
-    body: req.body.postBody
-  };
-  posts.push(post);
+    content: req.body.postBody
+  });
+
+  post.save();
   res.redirect("/");
-
-  
-});
-
-app.get("/posts/:postName", function(req,res) {
-  let parameter = req.params.postName;
-  for (var i = 0; i < posts.length; i++) {
-    if (_.lowerCase(posts[i].title) === _.lowerCase(parameter)) {
-      res.render("post", {title:posts[i].title, body:posts[i].body});
-    };
-  };
 });
 
 
 
+app.route("/posts/:postName")
+  .get(function(req,res) {
+    let postTitle = req.params.postName;
 
-
-
-
-
-
+    Post.findOne({title: postTitle})
+      .then((post) => {
+        if(post) {
+          res.render("post", {title:post.title, body:post.content});
+        }
+        else {
+          res.redirect("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  })
+  .put(function(req, res) {
+    let postTitle = req.params.postName;
+    Post.findOneAndUpdate(
+      { title: postTitle },
+      { title: req.body.postTitle, content: req.body.postBody },
+      { new: true }
+    )
+      .then((updatedPost) => {
+        res.render("post", { title: updatedPost.title, body: updatedPost.content });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .delete(function(req, res) {
+    let postTitle = req.params.postName;
+    Post.findOneAndDelete({ title: postTitle })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    });
 
 
 
